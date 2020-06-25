59f49839dcc9e0cb6514121d46c396d8109f2471
Danny-Gutierrez <danny.gutierrez@virginiogomez.cl>
2020-06-24 22:18:04 -0400

    vercion 1

---
+++ Clinica/Controllers/PacientesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Clinica.Controllers
+{
+    public class PacientesController : Controller
+    {
+        // GET: Pacientes
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult View(int id)
+        {
+            return View();
+        }
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ Clinica/Controllers/RolesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Clinica.Controllers
+{
+    public class RolesController : Controller
+    {
+        // GET: Pacientes
+        public ActionResult Index()
+        {
+            return View();
+        }
+     
+    }
+}
