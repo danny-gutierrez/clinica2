bcfc17165a81a86bda63a549edf0371f7048cf26
Danny-Gutierrez <danny.gutierrez@virginiogomez.cl>
2020-06-27 15:59:37 -0400

    cambio de controller y restricciones

---
+++ Clinica/Models/Paciente.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Clinica.Models
+{
+    public class Paciente
+    {
+        public int Id { get;  set; }
+
+        [Required]
+        [StringLength(25, MinimumLength = 5)]
+
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(25, MinimumLength = 5)]
+        public string Apellido { get; set; }
+
+        
+        [Required(ErrorMessage = "Este campo es requerido.")]
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*",
+            ErrorMessage = "Dirección de Correo electrónico incorrecta.")]
+        [StringLength(100, ErrorMessage = "Longitud máxima 100")]
+        [DataType(DataType.EmailAddress)]
+
+        public string Correo { get; set; }
+
+        [Required]
+        [StringLength(25, MinimumLength = 5)]
+        public string Direccion { get; set; }
+
+        [Required]
+        [StringLength(25, MinimumLength = 5)]
+        public string Rut { get; set; }
+
+        [Required]
+        [StringLength(10, MinimumLength = 9)]
+        public string Telefono { get; set; }
+
+        
+    }
+}
