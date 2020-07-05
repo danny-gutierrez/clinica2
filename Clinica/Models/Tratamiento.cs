adad6e4772238b5715ef43dfc9047117853f86de
andres2485 <67347368+andres2485@users.noreply.github.com>
2020-07-05 12:20:55 -0400

    Se agrego model tratamiento-crear

---
+++ Clinica/Models/Tratamiento.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Clinica.Models
+{
+    public class Tratamiento
+    {
+        public int Id { get; set; }
+
+
+        [Required]
+        [StringLength(25, MinimumLength = 5)]
+        public String Nombres { get; set; }
+
+
+
+        [Required]
+        [StringLength(25, MinimumLength = 5)]
+        public String Valor { get; set; }
+
+
+
+        [Required]
+        [StringLength(25, MinimumLength = 5)]
+
+        public String Especialistas { get; set; }
+    }
+}
