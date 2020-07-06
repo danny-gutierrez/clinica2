fc3c2a4bac6986bd62a7df3718626f5ebe5f892b
Danny-Gutierrez <danny.gutierrez@virginiogomez.cl>
2020-07-05 21:57:18 -0400

    modificacion del controller boletas parte model

--- Clinica/Controllers/BoletasController.cs
+++ Clinica/Controllers/BoletasController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Clinica.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,17 +15,41 @@ namespace Clinica.Controllers
         {
             return View();
         }
+        [HttpGet]
         public ActionResult Create()
         {
-            return View();
+            Boleta boleta = new Boleta();
+            return View(boleta);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Boleta boleta)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("View", "Boletas", new { id = 1 });
+            }
+            return View(boleta);
         }
         public ActionResult View(int id)
         {
             return View();
         }
+        [HttpGet]
         public ActionResult Edit(int id)
         {
-            return View();
+            Boleta boleta = new Boleta();
+            return View(boleta);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Boleta boleta)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Edit", "Boletas", new { id = 1 });
+            }
+            return View(boleta);
         }
     }
 }
