afbcfee9bc6c716619d527f7d67a553911e7712b
fabianzentorres <67809928+fabianzentorres@users.noreply.github.com>
2020-07-06 23:22:44 -0400

    Modelo Cita

---
+++ Clinica/Models/Cita.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Clinica.Models
+{
+    public class Cita
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(25, MinimumLength = 5)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(25, MinimumLength = 5)]
+        public string Apellido { get; set; }
+
+        [Required]
+        public int Fecha { get; set; }
+
+ 
+        [Required]
+        [StringLength(25, MinimumLength = 5)]
+        public string Odontologo { get; set; }
+
+        [Required]
+        [StringLength(25, MinimumLength = 5)]
+        public string Telefono { get; set; }
+
+        [Required]
+        [StringLength(25, MinimumLength = 5)]
+        public string Motivo { get; set; }
+    }
+}
