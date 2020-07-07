a9e100d2a65b285edf7e8fd7e10c9d83d67bede9
fabianzentorres <67809928+fabianzentorres@users.noreply.github.com>
2020-07-06 23:24:23 -0400

    Agregue Controlador Citas

--- Clinica/Controllers/CitasController.cs
+++ Clinica/Controllers/CitasController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Clinica.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,54 @@ namespace Clinica.Controllers
 {
     public class CitasController : Controller
     {
-        // GET: Odontologos
-        
+        // GET: Citas
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
         public ActionResult Create()
         {
-            return View();
+            Cita cita = new Cita();
+            return View(cita);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Cita cita)
+        {
+            if (ModelState.IsValid)
+            {
+                //Guardo en base de datos
+                //O mando Request a REST API
+                return RedirectToAction("Index", "Citas", new { id = 1 });
+            }
+
+            return View(cita);
         }
         public ActionResult View(int id)
         {
             return View();
         }
+
+        [HttpGet]
         public ActionResult Edit(int id)
         {
-            return View();
+
+            Cita cita = new Cita();
+            return View(cita);
         }
+
+        [HttpPost]
+        public ActionResult Edit(Cita cita)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Edit", "Citas", new { id = 1 });
+            }
+            return View(cita);
+        }
+
+
     }
 }
