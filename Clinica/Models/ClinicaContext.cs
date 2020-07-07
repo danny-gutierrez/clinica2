514a21e96bd27e1cd547478fb6fb1de841a13f14
Danny-Gutierrez <danny.gutierrez@virginiogomez.cl>
2020-07-06 23:04:57 -0400

    pueba git y modelbd

---
+++ Clinica/Models/ClinicaContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Clinica.Models
+{
+    public class ClinicaContext : DbContext
+
+    {
+        public DbSet<Paciente> Pacientes { get; set; }
+    }
+}
