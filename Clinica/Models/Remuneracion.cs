a6d4e8b92dbea2734f7efd755b35b3e58e20a480
fabianzentorres <67809928+fabianzentorres@users.noreply.github.com>
2020-07-06 23:28:06 -0400

    Model remuneración

---
+++ Clinica/Models/Remuneracion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Clinica.Models
+{
+    public class Remuneracion
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(25, MinimumLength = 5)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(25, MinimumLength = 5)]
+        public string Apellido { get; set; }
+
+
+        [Required]
+        [StringLength(25, MinimumLength = 5)]
+        public string Cargo { get; set; }
+
+        [Required]
+        public string Monto { get; set; }
+
+  
+    }
+}
